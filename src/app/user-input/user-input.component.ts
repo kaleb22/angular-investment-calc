7a9ecd907464178bde27cc7f0066b2784ab602e7
kaleb22 <kaleb22@hotmail.com>
2025-02-18 11:49:35 -0300

    feat(user-input): binding inputs with properties using ngModel

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss',
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  initialValue = signal('0');
+  anualValue = signal('0');
+  expectReturn = signal('6');
+  duration = signal('10');
+
+  onSubmit() {}
+}
